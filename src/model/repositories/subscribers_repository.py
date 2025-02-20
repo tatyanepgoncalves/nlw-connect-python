3d3459ba500ef3a1e2d3a91b1f55ceac2eb27d26
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-02-20 19:43:25 -0300

    :sparkles: feat: Impleting subscribers

---
+++ src/model/repositories/subscribers_repository.py
@@ -0,0 +1,32 @@
+from src.model.configs.connection import DBConnectionHandler
+from src.model.entitites.inscritos import Inscritos
+
+
+class SubscribersRepository:
+    def insert(self, subscriber_infos: dict) -> None:
+        with DBConnectionHandler() as db:
+            try:
+                new_subscriber = Inscritos(
+                    nome=subscriber_infos.get("name"),
+                    email=subscriber_infos.get("email"),
+                    link=subscriber_infos.get("link"),
+                    evento_id=subscriber_infos.get("evento_id")
+                )
+                db.session.add(new_subscriber)
+                db.session.commit()
+
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
+
+    def select_subscriber(self, email: str, evento_id: int) -> Inscritos:
+        with DBConnectionHandler() as db:
+            data = (
+                db.session
+                .query(Inscritos)
+                .filter(
+                    Inscritos.email == email, Inscritos.evento_id == evento_id
+                )
+                .one_or_none()
+            )
+            return data
