78bdadfd7934f28c454084d3c817df73370b9aa2
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-02-23 12:03:13 -0300

    :sparkles: feat: Adding new methods in subscribers_repo

--- src/model/repositories/subscribers_repository.py
+++ src/model/repositories/subscribers_repository.py
@@ -1,6 +1,8 @@
+from sqlalchemy import func, desc
 from src.model.configs.connection import DBConnectionHandler
 from src.model.entitites.inscritos import Inscritos
 from .interfaces.subscribers_repository import SubscribersRepositoryInterface
+
 
 class SubscribersRepository(SubscribersRepositoryInterface):
     def insert(self, subscriber_infos: dict) -> None:
@@ -30,3 +32,33 @@ class SubscribersRepository(SubscribersRepositoryInterface):
                 .one_or_none()
             )
             return data
+
+    def select_subscribers_by_link(self, link: str, event_id: int) -> list:
+        with DBConnectionHandler() as db:
+            data = (
+                db.session
+                .query(Inscritos)
+                .filter(
+                    Inscritos.link == link, Inscritos.evento_id == event_id
+                )
+                .all()
+            )
+            return data
+
+    def get_ranking(self, event_id: int) -> list:
+        with DBConnectionHandler() as db:
+            result = (
+                db.session
+                .query(
+                    Inscritos.link,
+                    func.count(Inscritos.id).label("total")
+                )
+                .filter(
+                    Inscritos.evento_id == event_id,
+                    Inscritos.link.isnot(None)
+                )
+                .group_by(Inscritos.link)
+                .order_by(desc("total"))
+                .all()
+            )
+            return result
--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -1,15 +1,17 @@
 import pytest
 from .subscribers_repository import SubscribersRepository
 
+
 @pytest.mark.skip("Insert in DB")
 def test_insert():
-  subscriber_info = {
-    "name": "meuNome222",
-    "email": "email222@email.com",
-    "evento_id": 2
-  }
-  subs_repo = SubscribersRepository()
-  subs_repo.insert(subscriber_info)
+    subscriber_info = {
+        "name": "Taty",
+        "email": "email222@email.com",
+        "evento_id": 3
+    }
+    subs_repo = SubscribersRepository()
+    subs_repo.insert(subscriber_info)
+
 
 @pytest.mark.skip("Select in DB")
 def test_select_subscriber():
@@ -19,3 +21,15 @@ def test_select_subscriber():
     subs_repo = SubscribersRepository()
     resp = subs_repo.select_subscriber(email, evento_id)
     print(resp.nome)
+
+@pytest.mark.skip("Select in DB")
+def test_ranking():
+    evento_id = 3
+    subs_repo = SubscribersRepository()
+    resp = subs_repo.get_ranking(evento_id)
+
+    print()
+    
+    for elem in resp:
+      print(f'Link: {elem.link}, total de inscritos: {elem.total}')
+
