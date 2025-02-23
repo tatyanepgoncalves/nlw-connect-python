b621d8b06dd378281b2f5b356f8dd0caa824ce08
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-02-23 10:25:43 -0300

    ✨ feat: Implementing controllers

--- src/validators/subscribers_creator_validator.py
+++ src/validators/subscribers_creator_validator.py
@@ -3,32 +3,18 @@ from cerberus import Validator
 
 def subscribers_creator_validator(request: any):
     body_validator = Validator({
-        "data":  {
+        "data": {
             "type": "dict",
             "schema": {
-                "name": "string",
-                "required": True,
-                "empty": False,
-            },
-            "email": {
-                "type": "string",
-                "required": True,
-                "empty": False
-            },
-            "link": {
-                "type": "string",
-                "required": False,
-                "empty": False
-            },
-            "evento_id": {
-                "type": "integer",
-                "required": True,
-                "empty": False
+                "name": { "type": "string", "required": True, "empty": False },
+                "email": { "type": "string", "required": True, "empty": False },
+                "link": { "type": "string", "required": False, "empty": False },
+                "evento_id": { "type": "integer", "required": True, "empty": False}
             }
         }
     })
-
+    
     response = body_validator.validate(request.json)
 
     if response is False:
-      print(body_validator.errors)
+        raise Exception(body_validator.errors) 
